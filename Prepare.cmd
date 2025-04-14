f6cdbd8aba71c2e12abc1003a137d97ad1fe315c
Andrew Bogard <andrew.bogard@advania.co.uk>
2025-04-14 10:17:56 +1000

    Tweak File Path

--- Prepare.cmd
+++ Prepare.cmd
@@ -1,4 +1,4 @@
 curl https://raw.githubusercontent.com/psit365/aub/refs/heads/master/SendDeviceInfo.ps1 -o SendDeviceInfo.ps1
 curl https://raw.githubusercontent.com/psit365/aub/refs/heads/master/Sysprep.cmd -o Sysprep.cmd
 
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0SendDeviceInfo.ps1"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File ".\SendDeviceInfo.ps1"
