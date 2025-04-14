ae2bcc92c32c238590785804600b99fcaf7cb5e7
Andrew Bogard <andrew.bogard@advania.co.uk>
2025-04-14 10:03:00 +1000

    Update URLs AGAIN

--- Prepare.cmd
+++ Prepare.cmd
@@ -1,5 +1,5 @@
-curl https://github.com/psit365/aub/blob/master/SendDeviceInfo.ps1 -o SendDeviceInfo.ps1
-curl https://github.com/psit365/aub/blob/master/Sysprep.cmd -o Sysprep.cmd
+curl https://raw.githubusercontent.com/psit365/aub/refs/heads/master/SendDeviceInfo.ps1 -o SendDeviceInfo.ps1
+curl https://raw.githubusercontent.com/psit365/aub/refs/heads/master/Sysprep.cmd -o Sysprep.cmd
 
 powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0SendDeviceInfo.ps1"
 
